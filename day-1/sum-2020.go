4a08d44107c5daf4f7aa9d7ffe4444da86e1461d
Patrick Aikens <paikens@gmail.com>
2020-12-03 16:06:08 -0500

    Add golang solution to day-1

---
+++ day-1/sum-2020.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	lines, err := readLines("input")
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+
+	// Check for the 2 digit sum
+	for i, n := range lines {
+		for _, m := range lines[i+1:] {
+			if n+m == 2020 {
+				fmt.Printf("Found 2 numbers that sum to 2020: %d+%d: The product is %d\n", n, m, n*m)
+			}
+			for _, o := range lines[i+2:] {
+				if n+m+o == 2020 {
+					fmt.Printf("Found 3 numbers that sum to 2020: %d+%d+%d: The product is %d\n", n, m, o, n*m*o)
+				}
+			}
+		}
+	}
+}
+
+func readLines(filename string) (lines []int64, err error) {
+	// Open the file
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Read the file into a slice, converting each line to an int64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		val, _ := strconv.ParseInt(scanner.Text(), 10, 32)
+		lines = append(lines, val)
+	}
+
+	// Return the slice sorted in reverse order
+	sort.Slice(lines, func(i, j int) bool { return lines[j] < lines[i] })
+	return lines, err
+}
